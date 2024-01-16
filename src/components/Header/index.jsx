98f67f83d2df0c741eaaab1ed20f1b5701ee8e76
AgeBK <age24@hotmail.com>
2024-01-16 18:33:52 +1100

    progress with autoComplete

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Autocomplete, TextField } from "@mui/material";
+import { createFilterOptions } from "@mui/material/Autocomplete";
 import useAutoComplete from "../../hooks/useAutoComplete";
 import { Link } from "react-router-dom";
 import Nav from "../Nav";
@@ -11,6 +12,7 @@ import styles from "./Header.module.css";
 const Header = () => {
   const [initial, setInitial] = useState([]);
   const [data, setData] = useState([]);
+  const [overlay, setOverlay] = useState(false);
 
   const maxResults = 10;
 
@@ -41,8 +43,8 @@ const Header = () => {
   });
 
   useEffect(() => {
-    let wineList = all.map(({ name, id }) => {
-      return { label: name, id };
+    let wineList = all.map(({ name, id, category, variety }) => {
+      return { label: name, id, category, variety };
     });
     console.log(wineList);
     setInitial(wineList);
@@ -53,21 +55,24 @@ const Header = () => {
 
   // .sort();
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    const { value } = e.target;
-    console.log(value);
-    //if (value.length < 3) {
-    const arr = initial.filter(({ label }) => {
-      console.log(label);
-      return label.toLowerCase().includes(value.toLowerCase());
-    });
-    console.log(arr);
-    setData(arr);
-    //}
+  // const handleChange = (e) => {
+  //   console.log(e.target.value);
+
+  //   //}
+  // };
+
+  const handleClick = () => {
+    setOverlay(true);
   };
+
+  const handleBlur = () => {
+    setOverlay(false);
+  };
+
   return (
     <header className={styles.header}>
+      <div className={overlay ? styles.overlay : ""}></div>
+
       <div className={styles.headerRow}>
         <div className={styles.logo}>
           <Link to="/">
@@ -81,35 +86,44 @@ const Header = () => {
         <h1 className={styles.hdr}>
           AK <span>FINE WINES</span>
         </h1>
-        {/* <div className={styles.searchCont}>
-          <input
-            placeholder="Search"
-            className={styles.searchInput}
-            {...bindInput}
-          />
-          {isBusy && <div className={styles.searchBusy}>Loading...</div>}
-          <ul {...bindOptions} className={styles.searchResults}>
-            {suggestions.map((_, index) => (
-              <li
-                className={selectedIndex === index ? styles.selected : null}
-                key={index}
-                {...bindOption}
-              >
-                <div>{suggestions[index].label}</div>
-              </li>
-            ))}
-          </ul>
-        </div> */}
 
         <Autocomplete
-          disablePortal
-          autoComplete={true}
-          autoHighlight={true}
+          // disablePortal
+          // autoComplete={true}
+          // autoHighlight={true}
+          onChange={(e, value) => console.log(e.target, value.title)}
           className={styles.autoComplete}
           options={data}
-          onInputChange={handleChange}
+          filterOptions={createFilterOptions({
+            limit: 7,
+          })}
+          renderOption={(props, { id, label, category, variety }) => {
+            return (
+              <li key={id} {...props}>
+                <Link
+                  to={`/${category.toLowerCase()}/${variety.toLowerCase()}/${id}`}
+                >
+                  <div className={styles.itemCont}>
+                    <div className={styles.itemImg}>
+                      <Img
+                        image={`wine/${id}.jpg`}
+                        imageStyle="autoComplete"
+                        imageAlt={label}
+                      />
+                    </div>
+                    <div className={styles.itemLabel}>{label}</div>
+                  </div>
+                </Link>
+              </li>
+            );
+          }}
           renderInput={(params) => (
-            <TextField label="What are you looking for?" {...params} />
+            <TextField
+              label="What are you looking for?"
+              {...params}
+              onClick={handleClick}
+              onBlur={handleBlur}
+            />
           )}
         ></Autocomplete>
         <div className={styles.cartCont}>
